15c3c587b73da7bd3fa24cdc63f8a6cdc5b81f61
jabo <arnoldjabo@gmail.com>
2025-10-17 14:14:42 +0200

    exercises 5

---
+++ GreatestOfThree.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class GreatestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        int max = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+        System.out.println("Greatest number is: " + max);
+    }
+}
