2573bfffe5b629c0f721999453ebe0252e155dc1
jabo <arnoldjabo@gmail.com>
2025-11-18 11:54:14 +0200

    class-creation-with-java

---
+++ object-oriented/Student.java
@@ -0,0 +1,11 @@
+public class Student{
+    int id;
+    String name;
+}
+class TestStudent{
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.id = 101;
+        System.out.println(s1.id);
+    }
+}
